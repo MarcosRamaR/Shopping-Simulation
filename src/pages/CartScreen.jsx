08f9c436d1295ad9f3f035e630cc2b0c4ca525cb
MarcosRamaR <marcos.rama.1994@gmail.com>
2025-10-07 18:25:03 +0200

    Initial table to show the cart

--- src/pages/CartScreen.jsx
+++ src/pages/CartScreen.jsx
@@ -2,6 +2,28 @@
 
 export const CartScreen = () => {
   return (
-    <div>CartScreen</div>
+    <>
+    <table className="table table-dark table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Price</th>
+          <th scope="col">Amount</th>
+          <th scope="col">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row"></th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </table>
+    <div>
+      <button className="btn btn-primary">Continue to payment</button>
+    </div>
+    </>
   )
 }
